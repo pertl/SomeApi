3aa18ba811b898cdbd4bedc3ad0281b7c7dc7c8d
pertl <pertl@gmx.org>
2018-12-12 01:04:38 +0100

    initial

---
+++ SomeApi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Npgsql;
+
+namespace SomeApi
+{
+  public static class Program
+  {
+    public static void Main(string[] args) {
+      var connString = "Host=localhost;Username=peter;Database=peter";
+
+      using (var conn = new NpgsqlConnection(connString)) {
+        conn.Open();
+
+        using (var cmd = new NpgsqlCommand("SELECT now()", conn)) {
+          // cmd.Parameters.AddWithValue("p", "Hello world");
+          cmd.ExecuteNonQuery();
+        }
+      }
+
+      CreateWebHostBuilder(args).Build().Run();
+    }
+
+    private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+      WebHost.CreateDefaultBuilder(args)
+             .UseStartup<Startup>();
+  }
+}
